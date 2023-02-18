af78d2c41cf31d17a077af4eacc05bb8d0f0908e
Mc28Y <Mmerichukwuukah4life@gmail.com>
2023-02-17 17:29:04 -0800

    Task 7

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
 
-/**
- * main - this is the main function
- * Reason: always 0
- */
-
-int main(void)
-
-{
-	int i;
-
-	for (i = 0 ; i < 10 ; i++)
-		putchar(i + '0');
-	putchar('\n');
-	return (0);
-}
